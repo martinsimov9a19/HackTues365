938a36c14ecfe1f589e0b2f72d301648f7b837db
martinsimov10a19 <martinsimov519@gmail.com>
2019-03-15 12:42:22 +0200

    Added player movement, fixed bugs with multiplayer

---
+++ HackTues365/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public Rigidbody2D rb;
+    private Vector2 movement;
+    private bool isJumping = false;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal") * speed;
+        //float vertical = Input.GetAxis("Vertical") * speed;
+        //movement = new Vector2(horizontal, gameObjecst.transform.position.y);
+
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetButtonDown("w") && isJumping == false)
+        {
+
+            rb.AddForce(new Vector2(0f, 100f));
+        }
+        rb.velocity = movement;
+        
+    }
+
+}
