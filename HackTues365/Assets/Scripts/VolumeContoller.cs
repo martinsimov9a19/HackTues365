709a9a2dce1700976aa46685bab41e91b788a3a1
martinsimov10a19 <martinsimov519@gmail.com>
2019-03-16 17:35:23 +0200

    Added volume control system

---
+++ HackTues365/Assets/Scripts/VolumeContoller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeContoller : MonoBehaviour
+{
+
+    public AudioSource audio;
+    public Slider slider;
+
+    
+    void Update()
+    {
+        if (slider.value >= 0) {
+            audio.volume = slider.value;
+        }
+    }
+}
