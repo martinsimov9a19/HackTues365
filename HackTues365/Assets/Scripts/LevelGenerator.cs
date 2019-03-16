fc52c03c392e8f9a953ae46fd7d9e3334d007f6b
martinsimov10a19 <martinsimov519@gmail.com>
2019-03-16 22:46:48 +0200

    Added so level generator can read pixel color, new scene for image generated levels

---
+++ HackTues365/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    public Texture2D LevelMap;
+
+
+
+    void Start()
+    {
+        LevelGenerate();
+    }
+
+    void LevelGenerate() {
+        for (int x = 0; x < LevelMap.width; x++) {
+            for (int y = 0; y < LevelMap.height; y++) {
+
+            }
+        }
+    }
+
+    void GenerateSprite(int x, int y) {
+        Color color = LevelMap.GetPixel(x, y);
+
+        if (color.a == 0) {
+            return;
+        }
+        //Check For pixel color and generate sprite
+    }
+}
