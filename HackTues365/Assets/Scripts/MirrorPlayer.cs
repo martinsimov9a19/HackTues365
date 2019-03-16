51e9d145ccbff1f377a3281fc4b58371944e2f6f
martinsimov10a19 <martinsimov519@gmail.com>
2019-03-16 11:52:33 +0200

    Added player mirror object

---
+++ HackTues365/Assets/Scripts/MirrorPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorPlayer : MonoBehaviour
+{
+    public GameObject Player;
+
+    void Update()
+    {
+        Vector2 PlayerPos = Player.transform.position;
+
+        gameObject.transform.position = new Vector2(PlayerPos.x * -1, PlayerPos.y);
+
+    }
+}
