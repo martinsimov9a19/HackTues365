2349b796c6e5e00d065829851c49b7d0c48443fb
Simitars <simitars292@gmail.com>
2019-03-17 02:22:08 +0200

    Added changes to previous level and next level buttons so you can't big levels

--- HackTues365/Assets/Scripts/ButtonManager.cs
+++ HackTues365/Assets/Scripts/ButtonManager.cs
@@ -6,19 +6,27 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public int LevelCount = 2;
     public void Menu(){
         SceneManager.LoadScene("MainMenu");
         
     }
     public void Restart()
     {
-        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-        
-        
+
     }
     public void NextLevel() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        if (SceneManager.GetActiveScene().name != "Level" + LevelCount)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public void PreviousLevel() {
+        if (SceneManager.GetActiveScene().name != "Level1")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
     }
 
 
